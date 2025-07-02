96af026a10bd67a588542bbe5f6c772a91a8a95d
kyrins <oky6850@gmail.com>
2025-07-02 19:14:30 +0700

    second commit

---
+++ ui/menu.gd
@@ -0,0 +1,13 @@
+extends Control
+
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://main_game.tscn")
+
+
+func _on_credits_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
