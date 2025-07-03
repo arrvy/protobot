4604cd3737dee6c8f5e093bf72195beb25357e7f
arrvy <nashr.wahyono@mhs.unsoed.ac.id>
2025-07-03 16:16:07 +0700

    Upload tiles disappearing feature

--- main_game.gd
+++ main_game.gd
@@ -1,1 +1,12 @@
 extends Node2D
+
+
+@onready var player = $"C-129"
+@onready var tilemap = $MainMap
+
+func _process(delta):
+	var mat := tilemap.material as ShaderMaterial
+	if mat:
+		# Kirim posisi layar (viewport) ke shader
+		var player_screen_pos = get_viewport().get_camera_2d().unproject_position(player.global_position)
+		mat.set_shader_parameter("player_pos", player_screen_pos)
