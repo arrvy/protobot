989f666a5144874b38e7681f23c43d86bcc44586
Faisal <mmvxiixix@gmail.com>
2025-07-05 11:03:21 +0700

    Jedu-ar! done

---
+++ entities/Jedu-ar!/jedu_ar!.gd
@@ -0,0 +1,23 @@
+extends "res://entities/C-129/c_129.gd"
+
+@onready var explosion_timer: Timer = $ExplosionTimer
+
+func _ready() -> void:
+	super()
+	
+	explosion_timer.start() 
+	explosion_timer.timeout.connect(on_explosion_timeout)
+
+func on_explosion_timeout():
+	var explosion_scene = preload("res://entities/Jedu-ar!/explode.tscn")
+	var explosion_instance = explosion_scene.instantiate()
+	explosion_instance.global_position = global_position
+	get_tree().current_scene.add_child(explosion_instance)
+
+	queue_free()
+	
+func handle_death_input():
+	pass
+	
+func suicide():
+	pass
