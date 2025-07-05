55437a3fc79c895efb0227aac4d26f904fd17ebb
Faisal <mmvxiixix@gmail.com>
2025-07-05 08:38:50 +0700

    aplod

--- entities/C-129/c_129.gd
+++ entities/C-129/c_129.gd
@@ -3,23 +3,15 @@ extends "res://entities/base_robot.gd"
 @onready var push_area = $PushDirection
 @onready var main_game: Node2D = $".."
 @onready var iron_door_top: StaticBody2D = $"../Interactable Holder/IronDoor Top"
+
 @onready var pin_input: StaticBody2D = $"../Interactable Holder/Pin Input"
-
-@onready var vent_top: StaticBody2D = $"../Interactable Holder/Vent Top"
-
-
-
-
 
 func _ready():
 	super()
 	
-	#var cam = get_tree().root.get_node("MainGame/Camera2D") 
-	#cam.set_zoom_target(Vector2(2, 2))  
+	var cam = get_tree().root.get_node("MainGame/Camera2D") 
+	cam.set_zoom_target(Vector2(2, 2))  
 	
-
-
-
 func _physics_process(delta):
 	super._physics_process(delta)
 	var dir = get_input_vector()
@@ -41,12 +33,10 @@ func interactable_detector():
 		0:
 			pass
 		1:
-			interaction_handle(iron_door_top,pin_input)
-		2:
-			interaction_handle(vent_top,vent_top)
+			interaction_handle(iron_door_top)
 			
 			
-func interaction_handle(interaction,emitter):
+func interaction_handle(interaction):
 	if Input.is_action_just_pressed("interact"):
 		interaction.on_interactable_activated()
-		emitter.interactable_component.collision_mask = 1
+		pin_input.interactable_component.collision_mask = 1
--- main_game.gd
+++ main_game.gd
@@ -4,16 +4,12 @@ extends Node2D
 @onready var player = $"C-129"
 @onready var tilemap = $MainMap
 @onready var pin_input: StaticBody2D = $"Interactable Holder/Pin Input"
-@onready var vent_top: StaticBody2D = $"Interactable Holder/Vent Top"
-
-
 var interaction : int
 
 func _ready() -> void:
-	pin_input.input_deactivated.connect(on_input_handler)
-	pin_input.input_activated.connect(on_input_handler)
-	vent_top.input_activated.connect(on_vent_handler)
-	vent_top.input_deactivated.connect(on_vent_handler)
+	pin_input.input_deactivated.connect(on_input_activated)
+	pin_input.input_activated.connect(on_input_activated)
+	
 func _process(delta):
 	var mat := tilemap.material as ShaderMaterial
 	if mat:
@@ -21,10 +17,6 @@ func _process(delta):
 		var player_screen_pos = get_viewport().get_camera_2d().unproject_position(player.global_position)
 		mat.set_shader_parameter("player_pos", player_screen_pos)
 		
-func on_input_handler():
+func on_input_activated():
 	interaction = pin_input.interaction_type
-	print(interaction)
-
-func on_vent_handler():
-	interaction = vent_top.interaction_type
 	print(interaction)
