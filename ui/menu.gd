1801de18a9e8abb296c4fee539cde4ed3b7aa603
kyrins <oky6850@gmail.com>
2025-07-05 20:25:14 +0700

    Update linking button

--- ui/menu.gd
+++ ui/menu.gd
@@ -1,13 +1,13 @@
 extends Control
 
 
-func _on_start_pressed() -> void:
+func _on_mulai_pressed() -> void:
 	get_tree().change_scene_to_file("res://main_game.tscn")
 
 
-func _on_credits_pressed() -> void:
+func _on_kredit_pressed() -> void:
 	pass # Replace with function body.
 
 
-func _on_exit_pressed() -> void:
+func _on_keluar_pressed() -> void:
 	get_tree().quit()
