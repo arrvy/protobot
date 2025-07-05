3958062ebfac38d29eab2a57782fdfdba84b14fd
arrvy <nashr.wahyono@mhs.unsoed.ac.id>
2025-07-06 02:35:10 +0700

    bakuaer

--- entities/bakuaer/bakuaer.gd
+++ entities/bakuaer/bakuaer.gd
@@ -1,1 +1,7 @@
-extends CharacterBody2D
+extends "res://entities/C-129/c_129.gd"
+
+func _ready():
+	super()
+	
+	#var cam = get_tree().root.get_node("MainGame/Camera2D") 
+	#cam.set_zoom_target(Vector2(7, 7))
